728d0c1ebdb6a890926d3326aea38dd2bb71d7e9
Nathan Heskia <nathan.heskia@gmail.com>
2019-05-18 21:54:50 -0700

    Answer q0-q3

--- hw1.sql
+++ hw1.sql
@@ -1,69 +1,129 @@
 DROP VIEW IF EXISTS q0, q1i, q1ii, q1iii, q1iv, q2i, q2ii, q2iii, q3i, q3ii, q3iii, q4i, q4ii, q4iii, q4iv, q4v;
 
 -- Question 0
-CREATE VIEW q0(era) 
+CREATE VIEW q0(era)
 AS
-  SELECT 1 -- replace this line
+  SELECT MAX(era)
+  FROM pitching
 ;
 
 -- Question 1i
 CREATE VIEW q1i(namefirst, namelast, birthyear)
 AS
-  SELECT 1, 1, 1 -- replace this line
+  SELECT namefirst, namelast, birthyear
+  FROM people
+  WHERE weight > 300;
 ;
 
 -- Question 1ii
 CREATE VIEW q1ii(namefirst, namelast, birthyear)
 AS
-  SELECT 1, 1, 1 -- replace this line
+  SELECT namefirst, namelast, birthyear
+  FROM people
+  WHERE namefirst
+  LIKE '% %';
 ;
 
 -- Question 1iii
 CREATE VIEW q1iii(birthyear, avgheight, count)
 AS
-  SELECT 1, 1, 1 -- replace this line
+  SELECT birthyear, AVG(height), COUNT(*)
+  FROM people
+  GROUP BY birthyear
+  ORDER BY birthyear;
 ;
 
 -- Question 1iv
 CREATE VIEW q1iv(birthyear, avgheight, count)
 AS
-  SELECT 1, 1, 1 -- replace this line
+  SELECT birthyear, AVG(height), COUNT(*)
+  FROM people
+  GROUP BY birthyear
+  HAVING AVG(height) > 70
+  ORDER BY birthyear;
 ;
 
 -- Question 2i
 CREATE VIEW q2i(namefirst, namelast, playerid, yearid)
 AS
-  SELECT 1, 1, 1, 1 -- replace this line
+  SELECT namefirst, namelast, people.playerid, yearid
+  FROM people
+  INNER JOIN halloffame
+  ON people.playerid = halloffame.playerid
+  WHERE inducted = 'Y'
+  ORDER BY halloffame.yearid DESC;
 ;
 
 -- Question 2ii
 CREATE VIEW q2ii(namefirst, namelast, playerid, schoolid, yearid)
 AS
-  SELECT 1, 1, 1, 1, 1 -- replace this line
+  SELECT namefirst, namelast, people.playerid, schools.schoolid, halloffame.yearid
+  FROM people
+  INNER JOIN halloffame
+  ON people.playerid = halloffame.playerid
+  INNER JOIN collegeplaying
+  ON people.playerid = collegeplaying.playerid
+  INNER JOIN schools
+  ON collegeplaying.schoolid = schools.schoolid
+  WHERE inducted = 'Y' and schoolstate = 'CA'
+  ORDER BY halloffame.yearid DESC, collegeplaying.schoolid, collegeplaying.playerid;
 ;
 
 -- Question 2iii
 CREATE VIEW q2iii(playerid, namefirst, namelast, schoolid)
 AS
-  SELECT 1, 1, 1, 1 -- replace this line
+  SELECT people.playerid, namefirst, namelast, schools.schoolid
+  FROM people
+  INNER JOIN halloffame
+  ON people.playerid = halloffame.playerid
+  LEFT JOIN collegeplaying
+  ON people.playerid = collegeplaying.playerid
+  LEFT JOIN schools
+  ON collegeplaying.schoolid = schools.schoolid
+  WHERE inducted = 'Y'
+  ORDER BY people.playerid DESC, collegeplaying.schoolid;
 ;
 
 -- Question 3i
 CREATE VIEW q3i(playerid, namefirst, namelast, yearid, slg)
 AS
-  SELECT 1, 1, 1, 1, 1 -- replace this line
+  SELECT batting.playerid, namefirst, namelast, yearid, ((h - h2b - h3b - hr) + 2 * h2b + 3 * h3b + 4 * hr)/CAST(ab AS float) AS sg
+  FROM batting
+  INNER JOIN people
+  ON batting.playerid = people.playerid
+  WHERE ab > 50
+  ORDER BY sg DESC
+  LIMIT 10;
 ;
 
 -- Question 3ii
 CREATE VIEW q3ii(playerid, namefirst, namelast, lslg)
 AS
-  SELECT 1, 1, 1, 1 -- replace this line
+  SELECT batting.playerid, namefirst, namelast, ((SUM(h) - SUM(h2b) - SUM(h3b) - SUM(hr)) + 2 * SUM(h2b) + 3 * SUM(h3b) + 4 * SUM(hr))/CAST(SUM(ab) AS float) as lslg
+  FROM batting
+  INNER JOIN people
+  ON batting.playerid = people.playerid
+  GROUP BY batting.playerid, namefirst, namelast
+  HAVING SUM(ab) > 50
+  ORDER BY lslg DESC
+  LIMIT 10;
 ;
 
 -- Question 3iii
 CREATE VIEW q3iii(namefirst, namelast, lslg)
 AS
-  SELECT 1, 1, 1 -- replace this line
+  SELECT namefirst, namelast, ((SUM(h) - SUM(h2b) - SUM(h3b) - SUM(hr)) + 2 * SUM(h2b) + 3 * SUM(h3b) + 4 * SUM(hr))/CAST(SUM(ab) AS float) as lslg
+  FROM batting
+  INNER JOIN people
+  ON batting.playerid = people.playerid
+  GROUP BY batting.playerid, namefirst, namelast
+  HAVING SUM(ab) > 50 AND ((SUM(h) - SUM(h2b) - SUM(h3b) - SUM(hr)) + 2 * SUM(h2b) + 3 * SUM(h3b) + 4 * SUM(hr))/CAST(SUM(ab) AS float) > (
+    SELECT ((SUM(h) - SUM(h2b) - SUM(h3b) - SUM(hr)) + 2 * SUM(h2b) + 3 * SUM(h3b) + 4 * SUM(hr))/CAST(SUM(ab) AS float) as lslg
+    FROM batting
+    GROUP BY playerid
+    HAVING playerid = 'mayswi01'
+  )
+  ORDER BY namefirst;
 ;
 
 -- Question 4i
@@ -93,4 +153,3 @@ AS
 CREATE VIEW q4v(team, diffAvg) AS
   SELECT 1, 1 -- replace this line
 ;
-
